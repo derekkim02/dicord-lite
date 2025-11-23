3fea89d3c81fdf677c5fc1cd50c74d9b430f90cc
Derek Kim <z5362831@ad.unsw.edu.au>
2025-11-24 01:37:28 +1100

    first commit

---
+++ src/main/java/com/derek/discordlite/DiscordliteApplication.java
@@ -0,0 +1,13 @@
+package com.derek.discordlite;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DiscordliteApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DiscordliteApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/derek/discordlite/DiscordliteApplicationTests.java
@@ -0,0 +1,13 @@
+package com.derek.discordlite;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DiscordliteApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
